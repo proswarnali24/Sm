e421bea5ce78bc6fb3b33120461af67b22528b1b
proswarnali24 <sornalisen18@gmail.com>
2026-02-28 18:54:47 +0530

    Remove CN_Assignments from Sm repo

--- CN_Assignments/3rd/frame.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-struct Frame {
-    long long arrival_ts = 0; // when frame enters queue
-    int bits = 0;             // frame length in bits
-    int src = -1;             // sender node id
-    int dst = -1;             // receiver node id
-    bool is_reply = false;    // true if server->client
-};
--- CN_Assignments/4th/frame.h
+++
@@ -1,5 +0,0 @@
-#ifndef FRAME_H
-#define FRAME_H
-#include "common.h"
-struct ChipFrame { vector<int> chips; };
-#endif // FRAME_H
